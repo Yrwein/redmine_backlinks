fdbaef14ca14601019128ae169f98e734edd6ccb
Josef Cech <czech.josef@gmail.com>
2016-04-16 12:17:52 +0000

    Migrated to Redmine 3.2

--- lib/backlinks.rb
+++ lib/backlinks.rb
@@ -3,7 +3,7 @@ module RedmineBacklinks
   def self.get(page)
     project = page.project
 
-    results_all_projects, = WikiPage.search(
+    results_all_projects = WikiPage.search_results(
       [
         "[[#{project.identifier}:#{page.title}]]",
         "[[#{project.identifier}:#{page.title}|",
@@ -12,11 +12,13 @@ module RedmineBacklinks
         "[[#{project.name}:#{page.title}|",
         "[[#{project.name}:#{page.title}#",
       ],
+      User.current,
       nil, # all projects
       :titles_only => false
     )
-    results_current_project, = WikiPage.search(
+    results_current_project = WikiPage.search_results(
       ["[[#{page.title}]]", "[[#{page.title}|", "[[#{page.title}#"],
+      User.current,
       project,
       :titles_only => false
     )
